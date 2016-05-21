109267de7bef4341058615f2823d21c7bb479ce7
dvoituron <dvoituron@outlook.com>
2016-05-21 15:29:46 +0200

    Sample for JourneeAgile.be demonstration (2016)

---
+++ DevApps.MyProject/SimpleMath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevApps.MyProject
+{
+    public class SimpleMath
+    {
+        public static double Divide(int a, int b)
+        {
+            //return a / b;
+
+            double result = (double)a / (double)b;
+            return Math.Round(result);
+        }
+
+        public static int Surface(int height, int width)
+        {
+            if (height < 0 || width < 0)
+                return 0;
+            else
+                return height * width;
+        }
+
+    }
+}
